2631f078d4524933d0a713be9b69c3829e5e8371
Rafael Ramirez <randre17@hotmail.com>
2017-01-17 14:34:31 -0500

    Actualizar

---
+++ SolProy1/Proy1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proy1.MvcApplication" Language="C#" %>
---
+++ SolProy1/Proy1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proy1.Startup))]
+namespace Proy1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
